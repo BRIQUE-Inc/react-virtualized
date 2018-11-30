72ac1b6e20078077b3361bde5ac62728bd21fa65
daengdaengLee <gunho1020@gmail.com>
2018-11-30 12:45:12 +0900

    TST: Make virtualized list default story

---
+++ stories/basic/virtualized-list-default.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { VirtualizedList } from '../../src';
+
+const _makeItems = idx => [...Array(40)].map((_, i) => ({ id: idx + i }));
+
+class VirtualizedListDefault extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idx: 0,
+      listItems: _makeItems(0),
+    };
+    this._renderItem = this._renderItem.bind(this);
+    this._onChangeIdx = this._onChangeIdx.bind(this);
+    this._update = this._update.bind(this);
+  }
+
+  render() {
+    const { _renderItem, _onChangeIdx } = this;
+    return (
+      <VirtualizedList
+        height={600}
+        width={400}
+        itemCount={10000}
+        renderItemCount={30}
+        itemHeight={40}
+        renderItem={_renderItem}
+        onChangeIdx={_onChangeIdx}
+      />
+    );
+  }
+
+  _renderItem(idx, style) {
+    const { idx: startIdx, listItems } = this.state;
+    const itemKey = idx - startIdx;
+    const item = listItems[itemKey];
+    const content = item && item.id;
+    return (
+      <div
+        key={idx}
+        style={{
+          ...style,
+          backgroundColor: '#ffffff',
+          border: '1px solid black',
+          boxSizing: 'border-box',
+        }}
+      >
+        {content}
+      </div>
+    );
+  }
+
+  _onChangeIdx(idx) {
+    this.setState({ idx }, this._update);
+  }
+
+  _update() {
+    const { idx } = this.state;
+    this.setState({ listItems: _makeItems(idx) });
+  }
+}
+
+export default VirtualizedListDefault;

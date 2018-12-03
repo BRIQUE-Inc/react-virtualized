fafaddd12a823bac6e5002fbf9260468543a7baf
daengdaengLee <gunho1020@gmail.com>
2018-12-03 12:45:09 +0900

    ENH: Make virtualized table vertical component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import VirtualizedList from './components/2-molecules/virtualized-list/virtualized-list';
 import VirtualizedTable from './components/2-molecules/virtualized-table/virtualized-table';
+import VirtualizedTableVertical from './components/2-molecules/virtualized-table-vertical/virtualized-table-vertical';
 
-export { VirtualizedList, VirtualizedTable };
+export { VirtualizedList, VirtualizedTable, VirtualizedTableVertical };
 
 export default VirtualizedTable;

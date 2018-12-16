c9825f61422d84bbdf53872931bb9a7f9a2d40d4
daengdaengLee <gunho1020@gmail.com>
2018-12-17 08:06:58 +0900

    BLD: Build dist version 0.1.3

---
+++ dist/components/1-atoms/scroller/scroller.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const Scroller = styled.div.attrs(({
+  width,
+  height
+}) => ({
+  style: {
+    width,
+    height
+  }
+}))`
+  position: relative;
+`;
+export default Scroller;
---
+++ dist/components/1-atoms/table-container/table-container.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const TableContainer = styled.div.attrs(({
+  width,
+  height
+}) => ({
+  style: {
+    width,
+    height
+  }
+}))`
+  overflow: auto;
+`;
+export default TableContainer;

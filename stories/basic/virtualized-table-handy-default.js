ad7098b14d025de8c5ae9fefc31f8a2efefa14d9
daengdaengLee <gunho1020@gmail.com>
2018-12-17 09:53:47 +0900

    TST: Add code sample inside virtualized-table-handy story

--- stories/basic/virtualized-table-handy-default.js
+++ stories/basic/virtualized-table-handy-default.js
@@ -44,6 +44,61 @@ class VirtualizedTableHandyDefault extends Component {
     } = this;
 
     return (
+      <div style={{ display: 'flex', width: '100%', height: '100%' }}>
+        <VirtualizedTableHandy
+          height={600}
+          width={400}
+          style={{ boxSizing: 'border-box', border: '1px solid red' }}
+          rows={rows}
+          cols={cols}
+          renderRowCount={16}
+          renderColCount={3}
+          itemHeight={40}
+          itemWidth={140}
+          isFixedRow={true}
+          isFixedCol={true}
+          fixedRowHeight={80}
+          fixedColWidth={40}
+          fixedCornerStyle={{
+            backgroundColor: '#ffffff',
+            border: '1px solid black',
+            boxSizing: 'border-box',
+          }}
+          renderItem={_renderItem}
+          renderFixedRowItem={_renderFixedRowItem}
+          renderFixedColItem={_renderFixedColItem}
+        />
+        <textarea
+          disabled
+          style={{ width: 0, flexGrow: 1 }}
+          value={`
+import React, { Component } from 'react';
+import { VirtualizedTableHandy } from '@brique/react-virtualized';
+
+class VirtualizedTableHandyDefault extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cols: [{column_objects}],
+      rows: [{row_objects}],
+    };
+
+    this._renderItem = this._renderItem.bind(this);
+    this._renderFixedRowItem = this._renderFixedRowItem.bind(this);
+    this._renderFixedColItem = this._renderFixedColItem.bind(this);
+  }
+
+  render() {
+    const {
+      state: { rows, cols },
+
+      _renderItem,
+      _renderFixedRowItem,
+      _renderFixedColItem,
+    } = this;
+
+    return (
       <VirtualizedTableHandy
         height={600}
         width={400}
@@ -67,6 +122,62 @@ class VirtualizedTableHandyDefault extends Component {
         renderFixedRowItem={_renderFixedRowItem}
         renderFixedColItem={_renderFixedColItem}
       />
+    );
+  }
+
+  _renderItem(row, col, rowIdx, colIdx, style) {
+    return (
+      <div
+        key={\`\${rowIdx},\${colIdx}\`}
+        style={{
+          ...style,
+          backgroundColor: '#ffffff',
+          border: '1px solid black',
+          boxSizing: 'border-box',
+        }}
+      >
+        {row && col && row[col.key]}
+      </div>
+    );
+  }
+
+  _renderFixedRowItem(col, colIdx, style) {
+    return (
+      <div
+        key={\`-1,\${colIdx}\`}
+        style={{
+          ...style,
+          backgroundColor: '#ffffff',
+          border: '1px solid black',
+          boxSizing: 'border-box',
+        }}
+      >
+        {col && col.value}
+      </div>
+    );
+  }
+
+  _renderFixedColItem(row, rowIdx, style) {
+    return (
+      <div
+        key={\`\${rowIdx},-1\`}
+        style={{
+          ...style,
+          backgroundColor: '#ffffff',
+          border: '1px solid black',
+          boxSizing: 'border-box',
+        }}
+      >
+        {rowIdx}
+      </div>
+    );
+  }
+}
+
+export default VirtualizedTableHandyDefault;
+        `}
+        />
+      </div>
     );
   }
 

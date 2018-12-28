cb4f0056aad2ddb8f522b2b39ac95fd07bcd3844
daengdaengLee <gunho1020@gmail.com>
2018-12-28 14:12:58 +0900

    ENH: Use table-cell comp in virtualized-table-vertical comp

--- src/components/2-molecules/virtualized-table-vertical/virtualized-table-vertical.js
+++ src/components/2-molecules/virtualized-table-vertical/virtualized-table-vertical.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { HEADER_CELL_BACKGROUND_COLOR } from '../../../assets/js/constants';
+import Scroller from '../../1-atoms/scroller/scroller';
+import TableCell from '../../1-atoms/table-cell/table-cell';
+import TableContainer from '../../1-atoms/table-container/table-container';
+
+/* ======= Component ======= */
+
+/* === Main === */
 
 class VirtualizedTableVertical extends Component {
+  // --- lifecycle functions --- //
+
   constructor(props) {
     super(props);
+
     this.state = {
       rowIdx: props.initRowIdx,
     };
+
+    // refs
     this._fixedRow = React.createRef();
+
+    // event handlers
     this._onScroll = this._onScroll.bind(this);
     this._afterChangeRowIdx = this._afterChangeRowIdx.bind(this);
   }
 
   render() {
     const {
-      _fixedRow,
-      _onScroll,
       props: {
         height,
         width,
@@ -27,13 +40,23 @@ class VirtualizedTableVertical extends Component {
         itemWidth,
         isFixedRow,
         fixedRowHeight,
+        renderRow,
         renderItem,
+        renderFixedRow,
         renderFixedRowItem,
         innerRef,
       },
+
       state: { rowIdx },
+
+      // refs
+      _fixedRow,
+
+      // event handlers
+      _onScroll,
     } = this;
 
+    // make fixed row
     let fixedRow = [];
     if (isFixedRow) {
       for (let j = 0; j < colCount; j++) {
@@ -44,28 +67,26 @@ class VirtualizedTableVertical extends Component {
         fixedRow.push(renderFixedRowItem(j, style));
       }
     }
-    fixedRow = (
-      <div
-        ref={_fixedRow}
-        style={{
-          position: 'absolute',
-          width: '100%',
-          height: `${fixedRowHeight}px`,
-          display: 'flex',
-          zIndex: 10,
-        }}
-      >
-        {fixedRow}
-      </div>
-    );
+    const fixedRowStyle = {
+      position: 'absolute',
+      width: '100%',
+      height: `${fixedRowHeight}px`,
+      display: 'flex',
+      zIndex: 10,
+    };
+    fixedRow = renderFixedRow(fixedRowStyle, fixedRow, _fixedRow);
 
+    // make rows
     const rows = [];
     for (let i = 0; i < renderRowCount; i++) {
+      // make row
       const row = [];
       const _rIdx = i + rowIdx;
       const top = isFixedRow
         ? _rIdx * itemHeight + fixedRowHeight
         : _rIdx * itemHeight;
+
+      // make items
       for (let j = 0; j < colCount; j++) {
         const style = {
           width: typeof itemWidth === 'string' ? itemWidth : `${itemWidth}px`,
@@ -73,54 +94,55 @@ class VirtualizedTableVertical extends Component {
         };
         row.push(renderItem(_rIdx, j, style));
       }
-      rows.push(
-        <div
-          key={_rIdx}
-          style={{
-            position: 'absolute',
-            top: `${top}px`,
-            zIndex: 2,
-            width: '100%',
-            height: `${itemHeight}px`,
-            display: 'flex',
-          }}
-        >
-          {row}
-        </div>,
-      );
+
+      const rowStyle = {
+        position: 'absolute',
+        top: `${top}px`,
+        zIndex: 2,
+        width: '100%',
+        height: `${itemHeight}px`,
+        display: 'flex',
+      };
+      rows.push(renderRow(_rIdx, rowStyle, row));
     }
 
     return (
-      <div
-        style={{ ...style, overflow: 'auto', width, height }}
+      <TableContainer
+        width={width}
+        height={height}
+        style={style}
         onScroll={_onScroll}
         ref={innerRef}
       >
-        <div
-          style={{
-            position: 'relative',
-            width: '100%',
-            height: `${itemHeight * rowCount}px`,
-          }}
-        >
+        <Scroller width="100%" height={`${itemHeight * rowCount}px`}>
           {isFixedRow && fixedRow}
           {rows}
-        </div>
-      </div>
+        </Scroller>
+      </TableContainer>
     );
   }
 
+  // --- event handlers --- //
+
   _onScroll({ target: { scrollTop } }) {
     const {
-      _fixedRow,
-      _afterChangeRowIdx,
       props: { itemHeight, isFixedRow, rowCount, renderRowCount },
+
       state: { rowIdx },
+
+      // refs
+      _fixedRow,
+
+      // other functions
+      _afterChangeRowIdx,
     } = this;
+
     if (isFixedRow) {
       _fixedRow.current.style.top = `${scrollTop}px`;
     }
+
     const currentRowIdx = Math.floor(scrollTop / itemHeight);
+
     if (rowIdx !== currentRowIdx) {
       const maxRowIdx = rowCount - renderRowCount;
       const updateRowIdx =
@@ -129,11 +151,15 @@ class VirtualizedTableVertical extends Component {
     }
   }
 
+  // --- other functions --- //
+
   _afterChangeRowIdx() {
     const {
       props: { onChangeRowIdx },
+
       state: { rowIdx },
     } = this;
+
     onChangeRowIdx(rowIdx);
   }
 }
@@ -146,31 +172,32 @@ VirtualizedTableVertical.defaultProps = {
   colCount: 10,
   renderRowCount: 10,
   initRowIdx: 0,
-  itemHeight: 40,
+  itemHeight: 35,
   itemWidth: 120,
   isFixedRow: false,
-  fixedRowHeight: 50,
+  fixedRowHeight: 40,
+  renderRow: (rowIdx, style, children) => (
+    <div key={rowIdx} style={style}>
+      {children}
+    </div>
+  ),
   renderItem: (rowIdx, colIdx, style) => (
-    <div
+    <TableCell
       key={`${rowIdx},${colIdx}`}
-      style={{
-        ...style,
-        backgroundColor: '#ffffff',
-        border: '1px solid black',
-        boxSizing: 'border-box',
-      }}
-    >{`${rowIdx}, ${colIdx}`}</div>
+      style={style}
+    >{`${rowIdx}, ${colIdx}`}</TableCell>
+  ),
+  renderFixedRow: (style, children, ref) => (
+    <div ref={ref} style={style}>
+      {children}
+    </div>
   ),
   renderFixedRowItem: (colIdx, style) => (
-    <div
+    <TableCell
       key={`-1,${colIdx}`}
-      style={{
-        ...style,
-        backgroundColor: '#ffffff',
-        border: '1px solid black',
-        boxSizing: 'border-box',
-      }}
-    >{`HEAD${colIdx}`}</div>
+      backgroundColor={HEADER_CELL_BACKGROUND_COLOR}
+      style={style}
+    >{`HEAD${colIdx}`}</TableCell>
   ),
   onChangeRowIdx: rowIdx => {},
   innerRef: el => {},
@@ -188,7 +215,9 @@ VirtualizedTableVertical.propTypes = {
   itemWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   isFixedRow: PropTypes.bool,
   fixedRowHeight: PropTypes.number,
+  renderRow: PropTypes.func,
   renderItem: PropTypes.func,
+  renderFixedRow: PropTypes.func,
   renderFixedRowItem: PropTypes.func,
   onChangeRowIdx: PropTypes.func,
   innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),

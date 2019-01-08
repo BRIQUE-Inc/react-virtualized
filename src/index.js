5267756eafae8291fd4abc0e7564870a5dae90d4
daengdaengLee <gunho1020@gmail.com>
2019-01-08 09:38:55 +0900

    ENH: Make virtualized-table-col-resize comp

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import TableCell from './components/1-atoms/table-cell/table-cell';
 import Filter from './components/2-molecules/filter/filter';
 import VirtualizedList from './components/2-molecules/virtualized-list/virtualized-list';
 import VirtualizedTable from './components/2-molecules/virtualized-table/virtualized-table';
+import VirtualizedTableColResize from './components/2-molecules/virtualized-table-col-resize/virtualized-table-col-resize';
 import VirtualizedTableHandy from './components/2-molecules/virtualized-table-handy/virtualized-table-handy';
 import VirtualizedTableVertical from './components/2-molecules/virtualized-table-vertical/virtualized-table-vertical';
 
@@ -14,6 +15,7 @@ export {
   Filter,
   VirtualizedList,
   VirtualizedTable,
+  VirtualizedTableColResize,
   VirtualizedTableHandy,
   VirtualizedTableVertical,
 };
